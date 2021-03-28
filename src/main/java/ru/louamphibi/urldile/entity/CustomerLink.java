cb5e4713e24c0343ee3828508184db9dde6e41f0
Alexey <54241974+lou-amphibi@users.noreply.github.com>
2021-03-28 17:38:54 +0300

    Update CustomerLink.java

--- src/main/java/ru/louamphibi/urldile/entity/CustomerLink.java
+++ src/main/java/ru/louamphibi/urldile/entity/CustomerLink.java
@@ -1,9 +1,5 @@
 package ru.louamphibi.urldile.entity;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
